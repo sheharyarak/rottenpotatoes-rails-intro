a81e1b0e4d11b644b1fded91126311e10b41cbe3
Sheharyar Khan <sheharyarak@berkeley.edu>
2021-10-16 03:26:03 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.distinct.pluck(:rating)
+    @ratings_to_show = params[:ratings].nil? ? [] : params[:ratings].keys
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
